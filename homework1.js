5186b55fbe27cdcc373f395bf1e0f18029c493ac
Etienne Santiago Samboni <etienne.samboni@uao.edu.co>
2024-08-13 18:02:10 -0500

    Add: First Methods - pop & push

---
+++ homework1.js
@@ -0,0 +1,48 @@
+// Array methods
+
+// Array.at()
+/*The at() method of Array instances takes an integer value and returns 
+* the item at that index, allowing for positive and negative integers.
+* Negative integers count back from the last item in the array. NEW INTEGRATION!!!
+*/
+
+let fruits = ["Apple", "Orange", "Plum"];
+
+let index = 1;
+console.log(`An index of ${index} returns ${array1.at(index)}`);
+
+index = -1;
+console.log(`An index of ${index} returns ${array1.at(index)}`);
+
+// Array.pop()
+/* The pop() method removes the last element from an array and returns 
+* that element. This method changes the length of the array.
+*/
+
+const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+
+console.log(plants.pop());
+
+console.log(plants);
+
+plants.pop();
+
+console.log(plants);
+
+//Array.push()
+/* The push() method of Array instances adds the specified elements 
+to the end of an array and returns the new length of the array.*/
+// Consideration: The call of Array.push(...) equals to Array[Array.length] = ... .
+
+const animals = ['pigs', 'goats', 'sheep'];
+
+const count = animals.push('cows');
+console.log(count);
+
+console.log(animals);
+
+animals.push('chickens', 'cats', 'dogs');
+console.log(animals);
+
+
+
