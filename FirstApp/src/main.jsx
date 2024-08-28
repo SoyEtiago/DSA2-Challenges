29b35238320a3e09642aaf57401cabb937c0e7fc
Etienne Santiago Samboni <etienne.samboni@uao.edu.co>
2024-08-28 16:30:02 -0500

    "Update components directory"

--- FirstApp/src/main.jsx
+++ FirstApp/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import FirstApp from './FirstApp'
+import FirstApp from './components/FirstApp'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
