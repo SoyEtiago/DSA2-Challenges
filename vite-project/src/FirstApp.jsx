e48a8ac1299c3a1008d38f776626772503115c8d
Etienne Santiago Samboni <etienne.samboni@uao.edu.co>
2024-08-28 17:01:26 -0500

    "Prop Validations"

--- vite-project/src/FirstApp.jsx
+++ vite-project/src/FirstApp.jsx
@@ -1,14 +1,24 @@
+import PropTypes from 'prop-types';
 import './App.css';
 
-const title = 'First App';
 // Props recibe un objecto - puede manejar Destructuring assignment {title}
-const FirstApp = ( props ) => {
+const FirstApp = ( {title, sum} ) => {
   return (
     <>
-    <h1> {props.title} </h1>
-    <span>10</span>
+    <h1> {title} </h1>
+    <span> {sum} </span>
     </>
   )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  sum: PropTypes.number.isRequired
+}
+
+FirstApp.defaultProps = {
+  title: 'No hay titulo',
+  sum: 300
 }
 
 export default FirstApp;
