161ebd3e26983289ff6205cbdf74a9d6d47f45bf
Etienne <etienne.samboni@uao.edu.co>
2024-09-25 16:54:51 -0500

    "Intial code Reto14"

--- vite-project/src/components/TodoApp.jsx
+++ vite-project/src/components/TodoApp.jsx
@@ -12,8 +12,29 @@ export const TodoApp = () => {
 
   return (
     <>
-      <h1>TodoApp</h1>
+      <h1>TodoApp: 10, <small>Pendientes: 2</small></h1>
       <hr />
+
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">Item 1</span>
+              <button className="btn btn-danger">Borrar</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <form>
+            <input type="text" className="form-control" />
+            <button className="btn btn-outline-primary mt-1">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
